1f900d5e24d3ba0686a52975cd153c4b2c048b4e
pwnsauce8 <32200668+pwnsauce8@users.noreply.github.com>
2022-10-22 18:34:01 +0200

    Refactor

--- ds-1/client/node.py
+++ ds-1/client/node.py
@@ -8,31 +8,31 @@ class NodeColor(Enum):
     GREEN = 'GREEN'
     RED = 'RED'
 
-
 class Node:
 
     def __init__(self, port):
         self._isCoordinator = False
         self._color = NodeColor.NONE
-        self.port = port
-        self.timeout = 1
-        self.hostname = socket.gethostname()
-        self.ip = socket.gethostbyname(self.hostname)
-        self.semafore = threading.Semaphore(1)
-        self.disabled = False
+        self._port = port
+        self._timeout = 1
+        self._hostname = socket.gethostname()
+        self._ip = socket.gethostbyname(self.hostname)
+        self._semafore = threading.Semaphore(1)
+        self._disabled = False
+        self._other_nodes = []
 
     def disable(self):
-        self.disabled = True
-        self.color = NodeColor.GRAY
+        self._disabled = True
+        self._color = NodeColor.GRAY
         print("Node [" + self.ip + "] was disabled")
     
     def enable(self):
-        self.disabled = False
-        self.color = NodeColor.NONE
+        self._disabled = False
+        self._color = NodeColor.NONE
         print("Node [" + self.ip + "] was enabled")
 
     def setcolor(self, color):
-        self.color = color
+        self._color = color
         print("Node [" + self.ip + "] color is " + color)
 
 
